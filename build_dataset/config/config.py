b3f38bc68577ca894fb98242e784ea0631ce0e40
juan <jpalacio@axletravel.com>
2019-08-28 02:08:46 -0500

    Created the basis for building dataset

---
+++ build_dataset/config/config.py
@@ -0,0 +1,18 @@
+# define the paths to the images directory
+DATA_PATH = "../../datasets"
+
+IMAGES_PATH = DATA_PATH + "kaggle_dogs_vs_cats/train"
+
+NUM_CLASSES = 2
+NUM_VAL_IMAGES = 1250 * NUM_CLASSES
+NUM_TEST_IMAGES = 1250 * NUM_CLASSES
+
+TRAIN_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
+VAL_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
+TEST_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
+
+MODEL_PATH = "output/alexnet_dogs_vs_cats.model"
+
+DATASET_MEAN = "output/dogs_vs_cats_mean.json"
+
+OUTPUT_PATH = "output"
