7f7c838383af959a8983de89a638765299483329
Ubuntu <ubuntu@ip-172-31-62-12.us-west-2.compute.internal>
2019-08-28 07:39:44 +0000

    Added GPU requirements and modified config file

--- build_dataset/config/config.py
+++ build_dataset/config/config.py
@@ -1,15 +1,15 @@
 # define the paths to the images directory
-DATA_PATH = "../../datasets"
+DATA_PATH = "../dataset"
 
-IMAGES_PATH = DATA_PATH + "kaggle_dogs_vs_cats/train"
+IMAGES_PATH = DATA_PATH + "/train"
 
 NUM_CLASSES = 2
 NUM_VAL_IMAGES = 1250 * NUM_CLASSES
 NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 
-TRAIN_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
-VAL_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
-TEST_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
+TRAIN_HDF5 = "../dataset/hdf5/train.hdf5"
+VAL_HDF5 = "../dataset/hdf5/val.hdf5"
+TEST_HDF5 = "../dataset/hdf5/test.hdf5"
 
 MODEL_PATH = "output/alexnet_dogs_vs_cats.model"
 
